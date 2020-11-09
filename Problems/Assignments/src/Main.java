7ee67e7112e095b16f5bab42d210a4999ae6c325
firestormDeveloper <52833779+firestormDeveloper@users.noreply.github.com>
2020-11-09 22:15:08 +0100

    Initial commit

---
+++ Problems/Assignments/src/Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+
+        // Assign values to these four variables
+        int one;
+        int two;
+        int three;
+        int four;
+
+        one= 1;
+        two= 2;
+        three=3;
+        four=4;
+
+        System.out.println(String.format("%d %d %d %d", one, two, three, four));
+    }
+}
---
+++ Problems/Guest list of a hotel/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        
+        String one = terminal.next();
+        String two = terminal.next();
+//        terminal.nextLine();
+        String three = terminal.next();
+        String four = terminal.next();
+        String five = terminal.next();
+        String six = terminal.next();
+//        terminal.nextLine();
+        String seven = terminal.next();
+        String eight = terminal.next();
+        
+        System.out.println(eight);
+        System.out.println(seven);
+        System.out.println(six);
+        System.out.println(five);
+        System.out.println(four);
+        System.out.println(three);
+        System.out.println(two);
+        System.out.println(one);
+    }
+}
